785d971c813b99aa02858cbb74662c24e35e5fbf
Shanmoweiung <yun60999@gmail.com>
2026-04-15 17:19:57 +0800

    练习程序

---
+++ Study class/note/test_4_8/test.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	//float a = 3 / 1.0;	
+//	//printf("%f\n", a);
+//	int arr[10] = { 0 };
+//	printf("%d\n", sizeof(arr));
+//
+//	return 0;
+//}
+
+int main()
+{
+	int a = 1;
+	int b = 3;
+	//if (a && b)
+	//{
+	//	printf("³É¹¦");
+	//}
+	int r = a < b ? a : b;
+	printf("%d\n", r);
+
+
+	return 0;
+}
